6fbd7648dafca3dd1ee07c337bb18cfbf660f964
doofyus <mikk.ratsep@gmail.com>
2018-09-13 14:17:25 +0200

    double bytes var; fixedWidthInt's binary initialiser

--- Sources/HMUtilities/Double+Extensions.swift
+++ Sources/HMUtilities/Double+Extensions.swift
@@ -19,32 +19,20 @@
 // licensing@high-mobility.com
 //
 //
-//  FixedWidthInteger+Extensions.swift
+//  Double+Extensions.swift
 //  HMUtilities
 //
-//  Created by Mikk Rätsep on 15/03/2018.
+//  Created by Mikk Rätsep on 11/09/2018.
+//  Copyright © 2018 High Mobility. All rights reserved.
 //
 
 import Foundation
 
 
-public extension FixedWidthInteger {
+public extension Double {
 
-    /// Converts the value to the number of bytes used to represent the value.
-    ///
-    /// *E.g.* UInt32 would return 4 bytes, Int64 would return 8 bytes.
+    /// The bit pattern of the double, using the *IEE 754* standard.
     var bytes: [UInt8] {
-        return Swift.stride(from: 0, to: bitWidth, by: 8).reversed().map { UInt8((self >> $0) & 0xFF) }
-    }
-
-
-    /// Initialise the integer with bytes.
-    ///
-    /// *E.g.* UInt32 would be initialised with *4 bytes*, UInt64 with *8 bytes*, etc...
-    init<C>(_ binary: C) where C : Collection, C.Element == UInt8 {
-        let bytesCount = Self(1).bitWidth / 8   // Workaround for self.bitWidth
-        let prefixedBytes = binary.bytes.prefix(bytesCount)
-
-        self = prefixedBytes.reduce(Self(0)) { ($0 << 8) + Self($1) }
+        return bitPattern.bytes
     }
 }
